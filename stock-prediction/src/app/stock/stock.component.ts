1df1e6e1acd305b15badea70e8a62fda7b5542f0
vinutha <vinutha0808@gmail.com>
2019-08-07 11:31:16 +0530

    day 3task

--- stock-prediction/src/app/stock/stock.component.ts
+++ stock-prediction/src/app/stock/stock.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import { StockService  } from "../stock.service";
+import { STOCK } from '../stock-data';
+import { stock } from '../stock';
 @Component({
   selector: 'app-stock',
   templateUrl: './stock.component.html',
   styleUrls: ['./stock.component.css']
 })
 export class StockComponent implements OnInit {
+  private currentstockName : stock;
+  private pastSearchValue : stock[] = [];
 
-  constructor() { }
+  constructor(private stockservice : StockService) { }
 
   ngOnInit() {
+  }
+  getstock(stockName : string ):void{
+
+
+    this.currentstockName = this.stockservice.getStock(stockName);
+    this.currentstockName.searchTime = (new Date()).toTimeString();
+    this.pastSearchValue.push(this.currentstockName);
   }
 
 }
